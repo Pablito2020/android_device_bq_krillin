f6c136a33b6f86d4661e38115e435996633953e0
Pablito2020 <socdelbarca72@gmail.com>
2017-09-11 14:56:24 +0100

    vendorsetup.sh: apply the patches without typing anything

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
+# Run device patches on the tree
+sh device/bq/krillin/patches/apply-patches.sh
+
+# Add lunch combos
 add_lunch_combo lineage_krillin-userdebug
 add_lunch_combo lineage_krillin-eng
 add_lunch_combo lineage_krillin-user
