e93a328e21552660e0a2c91dc012542eabef2ebe
Pablito2020 <socdelbarca72@gmail.com>
2017-09-16 13:31:19 +0100

    krillin: now when you do the envsetup.sh, the patches will be applied

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Run device patches on the tree
-sh device/bq/krillin/patches/apply-patches.sh
+sh device/bq/krillin/patches/patches.sh
 
 # Add lunch combos
 add_lunch_combo lineage_krillin-userdebug
