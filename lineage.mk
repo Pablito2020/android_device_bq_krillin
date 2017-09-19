97e5f60e131709e0453304aeb1c806d8d58a9e03
Pablito2020 <socdelbarca72@gmail.com>
2017-09-19 19:36:08 +0200

    lineage.mk: Clean and add Nightly build propierty

--- lineage.mk
+++ lineage.mk
@@ -1,22 +1,16 @@
-# Full base
+# Inherit needed stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Needed stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Call device specific makefile
 $(call inherit-product, device/bq/krillin/device.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Set LOCAL_PATH 
 LOCAL_PATH := device/bq/krillin
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2419427:user/release-keys PRIVATE_BUILD_DESC="krillin-user 7.1.2 MRA58M 2419427 release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="krillin"
-
-# IO Scheduler
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.io.scheduler=bfq    
+    PRODUCT_DEVICE="krillin"  
        
 PRODUCT_NAME := lineage_krillin
 PRODUCT_DEVICE :=krillin
@@ -32,9 +26,11 @@ TARGET_SCREEN_WIDTH := 540
 PRODUCT_AAPT_CONFIG := normal mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-# Inherit some common CM stuff.
-CM_BUILD :=krillin
+# Inherit some common CM/Lineage OS stuff.
+CM_BUILD := krillin
+CM_BUILDTYPE := NIGHTLY
 
+# Krillin specific propierties
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
@@ -45,5 +41,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
   persist.service.adb.enable=1 \
   persist.service.debuggable=1 \
   persist.sys.usb.config=mtp,adb
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
