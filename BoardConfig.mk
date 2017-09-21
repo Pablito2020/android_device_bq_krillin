b7efe63fd7bd4605d401d8b1a45e3ed847e65331
Pablito2020 <socdelbarca72@gmail.com>
2017-09-21 20:13:18 +0200

    BoardConfig.mk: Add cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ MTK_PLATFORM := mt6582
 MTK_PROJECT := krillin
 TARGET_KERNEL_SOURCE := kernel/bq/krillin
 TARGET_KERNEL_CONFIG := lineage_krillin_defconfig
-BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_CMDLINE := console=tty0 console=ttyMT3,921600n1 root=/dev/ram vmalloc=496M slub_max_order=0 slub_debug=0 androidboot.hardware=sprout androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
