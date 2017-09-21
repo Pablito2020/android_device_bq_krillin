ddbd5e943d698fda924514a34214e45b0d226c33
Pablito2020 <socdelbarca72@gmail.com>
2017-09-21 20:11:19 +0200

    RAM Disk: We're krillin! not sprout. This was because we always used sprout patches... Now it is time to have our own ramdisk!

--- device.mk
+++ device.mk
@@ -76,16 +76,16 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # RAMDISK
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.sprout_common.rc:root/init.sprout_common.rc \
+    $(LOCAL_PATH)/rootdir/init.krillin_common.rc:root/init.krillin_common.rc \
     $(LOCAL_PATH)/rootdir/sbin/multi_init:root/sbin/multi_init \
     $(LOCAL_PATH)/rootdir/init.protect.rc:root/init.protect.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.sprout.rc:root/ueventd.sprout.rc \
-    $(LOCAL_PATH)/rootdir/init.sprout.usb.rc:root/init.sprout.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.sprout.rc:root/init.sprout.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.krillin.rc:root/ueventd.krillin.rc \
+    $(LOCAL_PATH)/rootdir/init.krillin.usb.rc:root/init.krillin.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.krillin.rc:root/init.krillin.rc \
     $(LOCAL_PATH)/rootdir/init.daredevil-power.rc:root/init.daredevil-power.rc \
-    $(LOCAL_PATH)/rootdir/fstab.sprout:root/fstab.sprout
+    $(LOCAL_PATH)/rootdir/fstab.krillin:root/fstab.krillin
     
 # TWRP RECOVERY
 PRODUCT_COPY_FILES += \
