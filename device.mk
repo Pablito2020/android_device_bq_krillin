8b44c1af4c959bb12f40f6ecc433d61a9a14a305
Pablito2020 <socdelbarca72@gmail.com>
2017-09-21 22:57:12 +0200

    RIL: Use SproutRIL (data isn't working, but it seems that the type of data (gsm) is now recognised

--- device.mk
+++ device.mk
@@ -185,7 +185,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	dalvik.vm.image-dex2oat-Xmx=64m \
 	ro.dalvik.vm.native.bridge=0 \
 	ro.hardware=sprout \
-	ro.telephony.ril_class=MediaTekRIL \
+	ro.telephony.ril_class=SproutRIL \
 	ro.telephony.ril.config=fakeiccid 
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
